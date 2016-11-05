6e1194db89c6c04e76f8709f4e791982cf79319a
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-04 22:56:17 -0400

    Add vowel change method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,6 +1,16 @@
-# 1. swapping the first and last name .reverse
-# 2. vowel = "aeiou"
-# => 	if new_vowel == "u", then new_vowel = "a"
-
-vowel = "aeiou"
-vowel.split("")
+# Establish the vowels
+# Turn vowels into array 
+# Take argument, compare it with each value at each index 
+def vowel_change(letter)
+	vowel = "aeiou"
+	vowel_array = vowel.split("")
+	index = 0
+	until letter == vowel_array[index]
+		vowel_array[index += 1] 
+	end
+	if letter == vowel_array[4]
+		letter = vowel_array[0]
+	else
+		letter = vowel_array[index + 1] 
+	end
+end
