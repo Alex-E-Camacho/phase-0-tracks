a212e4f8713777b1826e2ebab2fade64dfdc880f
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-08 11:41:45 -0500

    Add puppy characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES ------------------------
+# Puppy Dog
+
+# CHARACTERISTICS ----------------
+# Paw size: small
+# Friendly: yes
+# Bark: high pitched
+# Name: varies
+# Color: varies
+# Breath: unique
+
+# BEHAVIOR -----------------------
+# yelp
+# lick
+# sniff
+# pee everywhere
+# chew everything 
