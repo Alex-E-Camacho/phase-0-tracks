e5b9461a8afcd8c3595525deaf7085679836861b
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-11 16:35:08 -0500

    Add method templates

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,18 @@
+class TodoList 	
+
+	def initialize
+		@TodoList = ["do the dishes,", "mow the lawn"]	
+	end 
+
+	def get_items
+		get_items = @TodoList 
+	end 	
+
+	def add_items(n) 		
+		@get_items << n 
+		return @get_items 	
+	end  	
+
+	def delete_items 	
+	end  
+end
