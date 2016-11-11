b0cb51447b575e5144bf501105ce27ef3e114b9e
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-10 19:53:38 -0500

    Add new santa instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,20 +1,51 @@
 class Santa
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		puts "Initializing Santa instance..."
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
-		print "That was a good type of #{cookie_type}!"
+		puts "That was a good type of #{cookie_type}!"
 	end
 
-	def initialize
-		puts "Initializing Santa instance..."
+	def ranking
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
+	def age
+		@age = 0
+	end
+
+	def gender
+		puts @gender
+	end
 end
 
-st_nick = Santa.new
+st_nick = Santa.new("boy", "hispanic")
 
-st_nick.speak
+# st_nick.speak
 
-st_nick.eat_milk_and_cookies("oreo")
+# st_nick.eat_milk_and_cookies("oreo")
+
+# puts st_nick.age
+
+# p st_nick.ranking 
+
+# puts st_nick.gender
+
+santas = []
+
+santas << Santa.new("man", "white")
+santas << Santa.new("man-ish", "white-ish")
+santas << Santa.new("boy", "hispanic")
+
+puts santas.gender
+
+
+
+
