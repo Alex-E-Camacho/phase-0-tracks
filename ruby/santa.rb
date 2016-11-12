144f0e3ee5cea11cb202d9268efd5b414a1a7eb7
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-12 13:10:23 -0500

    Add santa making loop

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,12 @@
 class Santa
-	attr_reader :gender, :ethnicity
+	attr_reader :gender, :ethnicity, :reindeer_ranking, :age 
 	attr_accessor :gender, :ethnicity, :age
 
-	def initialize(gender, ethnicity, age)
+	def initialize(age, gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		@age = 0
+		@age = age 
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		puts "Initializing Santa instance..."
 	end
 
@@ -17,19 +18,19 @@ class Santa
 		puts "That was a good type of #{cookie_type}!"
 	end
 
-	def ranking
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	end
+	# def ranking
+	# 	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	# end
 
 	# Setter methods
 	def celebrate_birthday	
 		@age = @age + 1
 	end
 
-	def get_mad_at(array, deer_name)
-	array.delete_at(array.index(deer_name))
-	array << deer_name
-	print array 
+	def get_mad_at(deer_name)
+	@reindeer_ranking.delete_at(@reindeer_ranking.index(deer_name))
+	@reindeer_ranking << deer_name
+	@reindeer_ranking 
 	end
 
 	def gender=(new_gender)
@@ -37,47 +38,45 @@ class Santa
 	end
 end
 
-st_nick = Santa.new("boy", "hispanic", @age)
+#st_nick = Santa.new("boy", "hispanic", @age)
 
-# st_nick.speak
-
-# st_nick.eat_milk_and_cookies("oreo")
-
-# puts st_nick.age
-
-# p st_nick.ranking 
-
-# puts st_nick.gender
-
-p st_nick.age
-
-p st_nick.celebrate_birthday
-
-p st_nick.age 
-
-print st_nick.gender 
-
-st_nick.gender = "girl"
-
-print st_nick.gender 
-
-print st_nick.ranking 
-
-st_nick.get_mad_at((st_nick.ranking), "Rudolph")
-
-print st_nick.ranking 
-
-# santas = []
-
-# santas << Santa.new("man", "white")
-# santas << Santa.new("man-ish", "white-ish")
-# santas << Santa.new("boy", "hispanic")
-
-# santas.each do |about|
-# 	puts about.gender
-# 	puts about.ethnicity
+gender_arr = ["male", "female", "gender fluid", "neutral gendered", "boy-ish" ]
 	
-# end
+age_arr = (1..140).to_a 
+	
+ethnicity_arr = ["white", "black", "hispanic", "asian", "middle eastern"]
+
+puts "How many Santa's would you like to make?"
+number = gets.chomp.to_i 
+
+num_of_santa = 0 
+
+until num_of_santa == number
+	santa = Santa.new(age_arr.sample, gender_arr.sample, ethnicity_arr.sample)
+	puts "This santa is a #{santa.age} year old #{santa.ethnicity} and #{santa.gender}."
+	num_of_santa += 1 
+end 
+
+
+# p st_nick.age
+
+# p st_nick.celebrate_birthday
+
+# p st_nick.age 
+
+# print st_nick.gender 
+
+# st_nick.gender = "girl"
+
+# print st_nick.gender 
+
+# st_nick.get_mad_at("Rudolph")
+
+# print st_nick.reindeer_ranking
+
+
+
+
 
 
 
