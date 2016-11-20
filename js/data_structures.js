36c1a9b2a3d0ac052f38c38880407dfd1beddedc
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-20 14:53:08 -0500

    Add comments

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,23 +1,23 @@
-// var colorsArray1 = ["blue", "muted orange", "pink", "yellow"];
+var colorsArray1 = ["blue", "muted orange", "pink", "yellow"];
 
-// var namesArray1 = ["Ed", "Jerry", "Lina", "George"]
+var namesArray1 = ["Ed", "Jerry", "Lina", "George"]
 
-// colorsArray1.push("calico")
+colorsArray1.push("calico")
 
-// namesArray1.push("Bob")
+namesArray1.push("Bob")
 
 // Iterate through each name or color array
 // Create a new variable for new object
 // Assign key to horse name and value to color
 
-// var horses = {} 
+var horses = {} 
 
-// for (var x = 0; x < namesArray1.length; x ++) {
+for (var x = 0; x < namesArray1.length; x ++) {
 
-// 	horses[namesArray1[x]] = colorsArray1[x];
-// }
+	horses[namesArray1[x]] = colorsArray1[x];
+}
 
-// console.log(horses)
+console.log(horses)
 
 function Car(year, model, color) {
 	console.log("Car starting...");
@@ -37,4 +37,4 @@ newCar.horn();
 var ford = new Car(2016, "Mustang", "black"); 
 
 
-Object.keys(ford).forEach(function(key) {console.log(key)})
+Object.keys(ford).forEach(function(key) {console.log(key)}) // Iterates through keys of data structure function
