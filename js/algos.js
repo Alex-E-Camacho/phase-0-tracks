7fbc1c3ed6b88eda86d95a924d4607e428a2d20f
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-21 10:01:18 -0500

    Add final function to print longest word

--- js/algos.js
+++ js/algos.js
@@ -1,21 +1,32 @@
 // Go through each item in an array and find its length
 // Convert those lengths into a new array
 // Sort the new array with the largest value at the first spot
-// Set a condition that if a word's length in the original array is equal to the first number (which will be the largest) in the
-// 	new array, it prints that word to the console
+// Set a condition that if a word's length in the original array is equal to the first number in the
+// 	new array (which will be the largest), it prints that word to the console
 
 function longestWord(array) {
 	var numArray = [];
 	array.forEach(function(item) {
 		numArray.push(item.length)
 	})
-	return numArray.sort(sortArray)
+	return printLongest(array, numArray.sort(sortArray))
 }
 
 function sortArray(a,b) {
 	return b - a;
 }
 
-var test_array = ["apple", "pie", "sundae", "ice cream"];
+function printLongest(wordArr, numArr) {
+	wordArr.forEach(function(value) {
+    if (value.length == numArr[0]) {
+    return console.log(value); }
+})
+}
 
-console.log(longestWord(test_array));
+var testArray = ["apple", "pie", "sundae", "ice cream"];
+
+var shoeBrands = ["Nike", "Reebok", "Adidas", "Keds", "Timberland"]
+
+longestWord(testArray);
+
+longestWord(shoeBrands);
