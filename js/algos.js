03615f9b5c6806f06decdd9d0ff70e8ada40e3a2
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-21 08:11:53 -0500

    Create working function that turns array into word length values

---
+++ js/algos.js
@@ -0,0 +1,17 @@
+// Go through each item in an array and find its length
+// Convert those lengths into a new array
+// Sort the new array with the largest value at the first spot
+// Set a condition that if a word's length in the original array is equal to the first number (which will be the largest) in the
+// 	new array, it prints that word to the console
+
+function longestWord(array) {
+	var numArray = [];
+	array.forEach(function(item) {
+		numArray.push(item.length)
+	})
+	return numArray
+}
+
+var test_array = ["apple", "pie", "sundae"];
+
+console.log(longestWord(test_array));
