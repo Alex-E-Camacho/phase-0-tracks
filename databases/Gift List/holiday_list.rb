89e527b780367360a06c162a2a94296f949bb3dc
Alex-E-Camacho <alex.e.camacho@gmail.com>
2016-11-27 17:33:26 -0500

    Add update gift/price methods

--- databases/Gift List/holiday_list.rb
+++ databases/Gift List/holiday_list.rb
@@ -21,4 +21,19 @@ def add_gift(db, new_gift)
 	db.execute("INSERT INTO list (gift) VALUES (?)", [new_gift])
 end
 
+def add_price(db, item_price)
+	db.execute("INSERT INTO list (price) VALUES (?)", [item_price])
+end
+
+def delete_row(db, deleted_name)
+	db.execute("DELETE FROM list WHERE name=?", [deleted_name])
+end
+
+def update_price(db, new_price, list_name)
+	db.execute("UPDATE list SET price=? WHERE name=?", [new_price, list_name])
+end
+
+def update_gift(db, new_gift, list_name)
+	db.execute("UPDATE list SET gift=? WHERE name=?", [new_gift, list_name])
+end
 #add_name(db, "john henry")
